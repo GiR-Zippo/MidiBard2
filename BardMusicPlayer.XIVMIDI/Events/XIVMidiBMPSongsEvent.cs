dc1ab34885a8536895665d4847f4821574843f33
GiR_Zippo <gir_zippo@hellokitty.com>
2026-09-07 13:42:03 +0200

    [XIVMIDI] Update library to latest one

---
+++ BardMusicPlayer.XIVMIDI/Events/XIVMidiBMPSongsEvent.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Copyright(c) 2026 GiR-Zippo
+ * Licensed under the GPL v3 license. See https://github.com/GiR-Zippo/LightAmp/blob/main/LICENSE for full license information.
+ */
+
+using BardMusicPlayer.XIVMIDI.IO;
+
+namespace BardMusicPlayer.XIVMIDI.Events
+{
+    public sealed class XIVMidiBMPSongsEvent : XIVMidiEvent
+    {
+        internal XIVMidiBMPSongsEvent(BMPResponseContainer.Root data, bool dynamic) : base(0, false)
+        {
+            EventType = GetType();
+            Songs = data;
+            DynamicLoad = dynamic;
+        }
+
+        public BMPResponseContainer.Root Songs { get; }
+        public bool DynamicLoad { get; }
+        public override bool IsValid() => true;
+    }
+}
---
+++ BardMusicPlayer.XIVMIDI/Events/XIVMidiXIVSongsEvent.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Copyright(c) 2026 GiR-Zippo
+ * Licensed under the GPL v3 license. See https://github.com/GiR-Zippo/LightAmp/blob/main/LICENSE for full license information.
+ */
+
+using BardMusicPlayer.XIVMIDI.IO;
+
+namespace BardMusicPlayer.XIVMIDI.Events
+{
+    public sealed class XIVMidiXIVSongsEvent : XIVMidiEvent
+    {
+        internal XIVMidiXIVSongsEvent(XIVMIDIResponseContainer.ApiResponse data, bool dynamic) : base(0, false)
+        {
+            EventType = GetType();
+            Songs = data;
+            DynamicLoad = dynamic;
+        }
+
+        public XIVMIDIResponseContainer.ApiResponse Songs { get; }
+
+        public bool DynamicLoad { get; }
+
+        public override bool IsValid() => true;
+    }
+}
